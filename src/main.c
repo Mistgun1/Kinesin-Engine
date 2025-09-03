3dcf2a8d90114e23cbc411e790baa71d6703e0db
Mistgun1 <assilfa123@gmail.com>
2025-09-03 22:53:16 +0000

    update

--- src/main.c
+++ src/main.c
@@ -58,7 +58,8 @@ int main(void) {
     glDeleteShader(fragmentShader);
     glUseProgram(shaderProgram);
 
-
+    glvertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     while(!glfwWindowShouldClose(window)){
         processInput(window);
