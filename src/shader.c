e057b5b2cc1dbc43e2d7e457ebdfbb1f54960f27
Mistgun1 <assilfa123@gmail.com>
2025-09-06 21:19:33 +0000

    added shader header file

---
+++ src/shader.c
@@ -0,0 +1,73 @@
+#include "shader.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void initShader(Shader *shader){
+
+    FILE *fragmentShaderFile = fopen(shader->fragmentPath, "r");
+    FILE *vertexShaderFile = fopen(shader->vertexPath, "r");
+
+    int fragmentFileSize = fseek(fragmentShaderFile, 0, SEEK_END);
+    char fragmentBuffer[fragmentFileSize];
+    char *fragmentShaderCode = fgets(fragmentBuffer, sizeof(fragmentBuffer), fragmentShaderFile);
+
+    int vertexFileSize = fseek(vertexShaderFile, 0, SEEK_END);
+    char vertexBuffer[vertexFileSize];
+    char *vertexShaderCode = fgets(vertexBuffer, sizeof(vertexBuffer), vertexShaderFile);
+
+    fclose(fragmentShaderFile);
+    fclose(vertexShaderFile);
+
+    int vertex, fragment;
+    int success;
+    char infoLog[512];
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vertexShaderCode, NULL);
+    glCompileShader(vertex);
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+        printf("ERROR::VERTEX::%s\n", infoLog);
+    }
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fragmentShaderCode, NULL);
+    glCompileShader(fragment);
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+        printf("ERROR::FRAGMENT::%s\n", infoLog);
+    }
+    
+    shader->ID = glCreateProgram();
+    glAttachShader(shader->ID, vertex);
+    glAttachShader(shader->ID, fragment);
+    glLinkProgram(shader->ID);
+    glGetProgramiv(shader->ID, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(shader->ID, 512, NULL, infoLog);
+        printf("ERROR::PROGRAM::%s\n", infoLog);
+    }
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+void useShader(Shader *shader){
+    glUseProgram(shader->ID);
+}
+
+void setBool(Shader *shader, char *name, bool value){
+    glUniform1i(glGetUniformLocation(shader->ID, name), (int)value);
+}
+
+
+void setFloat(Shader *shader, char *name, float value){
+    glUniform1i(glGetUniformLocation(shader->ID, name), value);
+}
+
+void setInt(Shader *shader, char *name, int value){
+    glUniform1i(glGetUniformLocation(shader->ID, name), value);
+}
--- src/shader.h
+++ src/shader.h
@@ -2,8 +2,21 @@
 #define SHADER_H
 
 #include "se_gl.h"
-#include <iostream>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+typedef struct Shader{
+    unsigned int ID;
+    char *vertexPath;
+    char *fragmentPath;
+}Shader;
+
+void initShader(Shader *shader);
+void useShader(Shader *shader);
+void setBool(Shader *shader, char *name, bool value);
+void setInt(Shader *shader, char *name, int value);
+void setFloat(Shader *shader, char *name, float value);
 
 #endif
